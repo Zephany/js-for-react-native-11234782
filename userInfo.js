098fafb77dd56640b66f71cbb896af2ea1a464f3
Zephany <zephanilaopoku4@gmail.com>
2024-05-17 14:20:23 +0000

    Task 3 added

---
+++ userInfo.js
@@ -0,0 +1,8 @@
+function createUserProfiles(names,modifiedNames) {
+    return names.map((name, index)=> ({
+        id: index + 1,
+        originalName: name,
+        modifiedName: modifiedNames[index],
+    })
+)
+}
