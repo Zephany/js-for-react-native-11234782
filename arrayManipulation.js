950242e739a4909e9215a5024f82993e0b989dcc
Zephany <zephanilaopoku4@gmail.com>
2024-05-17 14:17:21 +0000

    Task 1 and 2 added

---
+++ arrayManipulation.js
@@ -0,0 +1,21 @@
+function processArray(arr) {
+    return arr.map 
+    
+    (num => {
+        if (num%2 ===0) {return num*num}
+        else{return num*3}
+    })
+}
+
+
+function formatArrayStrings(strings,numbers) {
+    return strings.map((str, index) => {
+        if(numbers[index] %2===0) {
+            return str.toUpperCase();    
+        }
+        else{
+            return str.toLowerCase();
+        }
+    }
+)
+}
